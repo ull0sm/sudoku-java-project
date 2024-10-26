196aad480a9faf37ec3f57fb3950853a1f0d084f
ull0sm <ullas4101997@gmail.com>
2024-10-26 01:28:21 -0400

    first prototype

---
+++ Board.java
@@ -0,0 +1,22 @@
+public class Board {
+    private static final int SIZE = 9; // Size of the Sudoku board
+    private int[][] board;
+
+    public Board(int[][] board) {
+        this.board = board;
+    }
+
+    // Method to display the board
+    for (int row = 0; row < SIZE; row++) {
+            if (row % 3 == 0 && row != 0) {
+                System.out.println("- - - - - - - - - - - - - - - -");
+            }
+            for (int col = 0; col < SIZE; col++) {
+                if (col % 3 == 0 && col != 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+}
